70d0eab8770b34fcb1731101081e17ab42dc31dd
kevinazevedo <taylorvision95@outlook.com>
2024-11-13 01:24:26 -0300

    Autenticando usuário

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,24 @@
+<?php
+    // variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
+
+    // usuários do sistema 
+    $usuarios_app = array(
+        array("email" => "adm@gmail.com", "senha" => "adm"),
+        array("email" => "dev@gmail.com", "senha" => "102030"),
+    );
+
+    foreach ($usuarios_app as $user) {
+        if ($user["email"] == $_POST["email"] && $user["senha"] == $_POST["senha"]) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if ($usuario_autenticado) {
+        echo "Usuário autenticado";
+    }
+
+    else {
+        header("location: index.php?login=erro");
+    }
+?>
