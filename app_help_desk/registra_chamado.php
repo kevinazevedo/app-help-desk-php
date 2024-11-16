89b0605bf9bdd069538763ba8bff251cd3cf2162
kevinazevedo <taylorvision95@outlook.com>
2024-11-16 20:46:42 -0300

    Registrando chamados

---
+++ app_help_desk/registra_chamado.php
@@ -0,0 +1,18 @@
+<?php 
+    // trabalhando na montagem do texto
+    $titulo = str_replace("#", "-", $_POST["titulo"]);
+    $categoria = str_replace("#", "-", $_POST["categoria"]);
+    $descricao = str_replace("#", "-", $_POST["descricao"]);
+    $texto = $titulo . "#" . $categoria . "#" . $descricao . PHP_EOL;
+
+    // abrindo o arquivo
+    $arquivo = fopen("arquivo.txt", "a");
+    
+    // escrevendo o texto
+    fwrite($arquivo, $texto);
+   
+    // fechando o arquivo
+    fclose($arquivo);
+
+    header("location: abrir_chamado.php");
+?>
