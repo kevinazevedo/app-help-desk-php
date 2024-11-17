ab9531105715ce4ba7a99e91bc7d548abc5b728c
kevinazevedo <taylorvision95@outlook.com>
2024-11-16 22:20:31 -0300

    Consultando chamados

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -2,6 +2,23 @@
   require_once("validador_acesso.php");
 ?>
 
+<?php
+  // chamados
+  $chamados = array();
+
+  // abrindo o arquivo
+  $arquivo = fopen("arquivo.txt", "r");
+
+  // lendo as linhas do arquivo
+  while(!feof($arquivo)) {
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  // fechando o arquivo
+  fclose($arquivo);
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -44,23 +61,29 @@
             
             <div class="card-body">
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <?php
+                foreach($chamados as $chamado) {;
+              ?>
 
+                <?php 
+                  $chamado_dados = explode("#", $chamado);
+
+                  if (count($chamado_dados) < 3) {
+                    continue;
+                  }
+                ?>
+
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                    <p class="card-text"><?= $chamado_dados[2] ?></p>
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php 
+                } 
+              ?>  
 
               <div class="row mt-5">
                 <div class="col-6">
