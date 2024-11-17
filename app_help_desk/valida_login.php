a5012f49b01148554a8ff2dbd7d0fcc1f143c0a6
kevinazevedo <taylorvision95@outlook.com>
2024-11-17 00:41:48 -0300

    Controle de perfil de usuários

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -3,21 +3,30 @@
 
     // variável que verifica se a autenticação foi realizada
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+    $perfis = array(1 => "Administrativo", 2 => "Usuário");
 
     // usuários do sistema 
     $usuarios_app = array(
-        array("email" => "adm@gmail.com", "senha" => "adm"),
-        array("email" => "dev@gmail.com", "senha" => "102030"),
+        array("id" => 1, "email" => "adm@adm.com", "senha" => "adm", "perfil_id" => 1),
+        array("id" => 2, "email" => "adm2@adm.com", "senha" => "adm", "perfil_id" => 1),
+        array("id" => 3, "email" => "michael@user.com", "senha" => "123", "perfil_id" => 2),
+        array("id" => 4, "email" => "james@user.com", "senha" => "123", "perfil_id" => 2)
     );
 
     foreach ($usuarios_app as $user) {
         if ($user["email"] == $_POST["email"] && $user["senha"] == $_POST["senha"]) {
             $usuario_autenticado = true;
+            $usuario_id = $user["id"];
+            $usuario_perfil_id = $user["perfil_id"];
         }
     }
 
     if ($usuario_autenticado) {
         $_SESSION["autenticado"] = "SIM";
+        $_SESSION["id"] = $usuario_id;
+        $_SESSION["perfil_id"] = $usuario_perfil_id;
         header("location: home.php");
     }
 
